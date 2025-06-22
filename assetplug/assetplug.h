d6c061aa18b8a7d876d6ea8b89f80aa9c8830c3f
UncStatus++ <164366136+Unc-Status@users.noreply.github.com>
2025-06-22 18:07:12 -0500

    Update assetplug.h

--- assetplug/assetplug.h
+++ assetplug/assetplug.h
@@ -5,6 +5,27 @@
 #ifndef ASSETPLUG_H
 #define ASSETPLUG_H
 
-#define 
+#define ASSET_MAJOR "Asset Plugin"
+
+class AssetPlug{
+public:
+   AssetPlug();
+   virtual ~AssetPlug();
+
+   /**
+    What Number Model It Is
+   */
+   int m_AssetNumberId;
+
+   /**
+    Asset Was Plugged In
+   */
+   bool bPluggedIn(const char * pAsset);
+
+   /**
+    Plugin Message Name
+   */
+   const char * cAssetPluginName = "Asset Plugin %5.2c\n";
+};
 
 #endif
